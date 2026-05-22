9a989b5c0902e349af1d3976dfa27cbd0381ff4c
Aneesh <aneeshcheriank@gmail.com>
2026-05-22 23:00:22 +0200

    llm initialization rectified

--- src/model.py
+++ src/model.py
@@ -5,17 +5,18 @@ import os
 from langchain_deepseek import ChatDeepSeek
 
 from src.config_env import config_env
-from src.config import api_key
+from src.config import api_key_var
 
-config_env()
+
 
 def get_llm():
     config_env()
+    api_key = os.environ.get(api_key_var)
 
     return ChatDeepSeek(
         model="deepseek-v4-flash",
         temperature=0.0,
-        api_key=os.environ.get("api_key"),
+        api_key=api_key,
         extra_body={"thinking": { # to disable the reasoning (creates problems in formating tool)
             "type": "disabled"
         }} 
